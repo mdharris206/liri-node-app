78815b50986fd4fe53b5c544fa95c1f63ff09b54
mdh206 <49321381+mdharris206@users.noreply.github.com>
2019-05-14 23:49:30 -0400

    submit homework

---
+++ keys.js
@@ -0,0 +1,9 @@
+// var keys = require("./keys.js");
+// var spotify = new Spotify(keys.spotify);
+
+console.log('this is loaded');
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
